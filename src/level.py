ea400ca98754d8f1daae35260dff9e53401c1972
Wicked <wwicked@o2.pl>
2021-01-26 16:28:40 +0100

    Add drawing text, blocks, loading assets and some minor changes

--- src/level.py
+++ src/level.py
@@ -18,21 +18,24 @@ class Level:
                 x = maze_x * self.cfg.block_size
                 y = maze_y * self.cfg.block_size
 
-                # Draw text fields (start and end)
                 if field in ["s", "e"]:
-                    font = self.cfg.font[field]
-                    text = pygame.font.SysFont(font["name"], font["size"])
-                    source = text.render(font["text"], True, font["color"])
-
-                    # Center text inside the block that it's in
-                    bs = self.cfg.block_size / 2
-                    rect = source.get_rect(center=(x + bs, y + bs))
-
-                    # Finally draw the text
-                    surface.blit(source, rect)
-
-                # Draw blocks
+                    self.draw_text(surface, field, x, y)
                 else:
-                    if field in self.cfg.assets["blocks"].keys():
-                        image = self.cfg.assets["blocks"][field]
-                        surface.blit(image, (x, y))
+                    self.draw_block(surface, field, x, y)
+
+    def draw_block(self, surface, field, x, y):
+        if field in self.cfg.assets["blocks"].keys():
+            image = self.cfg.assets["blocks"][field]
+            surface.blit(image, (x, y))
+    
+    def draw_text(self, surface, field, x, y):
+        font = self.cfg.font[field]
+        text = pygame.font.SysFont(font["name"], font["size"])
+        source = text.render(font["text"], True, font["color"])
+
+        # Center text inside the block that it's in
+        bs = self.cfg.block_size / 2
+        rect = source.get_rect(center=(x + bs, y + bs))
+
+        # Finally draw the text
+        surface.blit(source, rect)
